ba10f655cda978501466f5830fec562513b008fa
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-10 20:37:30 +0300

    Update CollectionsExtensions.List.cs

--- Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsExtensions.List.cs
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsExtensions.List.cs
@@ -6,6 +6,20 @@ namespace UGF.RuntimeTools.Runtime.Collections
 {
     public static partial class CollectionsExtensions
     {
+        public static TValue Get<TValue, TArgument>(this List<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection, argument, predicate, (list, index) => list[index], collection.Count, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TValue, TArgument>(this List<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection, argument, predicate, (list, index) => list[index], collection.Count, out result);
+        }
+
         public static TValue Get<TValue, TArgument>(this IReadOnlyList<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
         {
             if (collection == null) throw new ArgumentNullException(nameof(collection));
