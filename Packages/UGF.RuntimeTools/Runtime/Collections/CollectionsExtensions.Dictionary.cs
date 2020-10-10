eb2e824203f8f9cb2c87ac35dd6440022fa4bb75
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-10 20:31:42 +0300

    Change CollectionsExtensions

--- Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsExtensions.Dictionary.cs
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsExtensions.Dictionary.cs
@@ -6,6 +6,34 @@ namespace UGF.RuntimeTools.Runtime.Collections
 {
     public static partial class CollectionsExtensions
     {
+        public static TKey Get<TKey, TValue, TArgument>(this Dictionary<TKey, TValue>.KeyCollection collection, TArgument argument, CollectionPredicate<TKey, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TKey result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TKey, TValue, TArgument>(this Dictionary<TKey, TValue>.KeyCollection collection, TArgument argument, CollectionPredicate<TKey, TArgument> predicate, out TKey result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
+        public static TValue Get<TKey, TValue, TArgument>(this Dictionary<TKey, TValue>.ValueCollection collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TKey, TValue, TArgument>(this Dictionary<TKey, TValue>.ValueCollection collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
         public static TValue Get<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> collection, TKey key)
         {
             if (collection == null) throw new ArgumentNullException(nameof(collection));
---
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsExtensions.Misc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UGF.RuntimeTools.Runtime.Collections
+{
+    public static partial class CollectionsExtensions
+    {
+        public static TValue Get<TValue, TArgument>(this HashSet<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TValue, TArgument>(this HashSet<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
+        public static TValue Get<TValue, TArgument>(this Stack<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TValue, TArgument>(this Stack<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
+        public static TValue Get<TValue, TArgument>(this Queue<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TValue, TArgument>(this Queue<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
+        public static TValue Get<TValue, TArgument>(this LinkedList<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out TValue result) ? result : throw new CollectionValueNotFoundByArgumentException(argument);
+        }
+
+        public static bool TryGet<TValue, TArgument>(this LinkedList<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return CollectionsUtility.TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+    }
+}
