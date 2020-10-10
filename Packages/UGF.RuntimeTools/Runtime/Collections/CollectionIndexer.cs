30b3eca3ede8a44ef34490dff8dca8f2f129ddac
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-10 20:10:23 +0300

    Change CollectionsUtility and CollectionsExtensions

---
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionIndexer.cs
@@ -0,0 +1,4 @@
+﻿namespace UGF.RuntimeTools.Runtime.Collections
+{
+    public delegate TValue CollectionIndexer<out TValue, in TCollection>(TCollection collection, int index);
+}
--- Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsUtility.cs
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsUtility.cs
@@ -1,121 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace UGF.RuntimeTools.Runtime.Collections
 {
     public static class CollectionsUtility
     {
+        public static bool TryGet<TValue, TArgument, TCollection>(TCollection collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, CollectionIndexer<TValue, TCollection> indexer, int count, out TValue result)
+        {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            if (indexer == null) throw new ArgumentNullException(nameof(indexer));
+
+            for (int i = 0; i < count; i++)
+            {
+                TValue element = indexer(collection, i);
+
+                if (predicate(element, argument))
+                {
+                    result = element;
+                    return true;
+                }
+            }
+
+            result = default;
+            return false;
+        }
+
         public static bool TryGet<TValue, TArgument>(IEnumerable<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
         {
             if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return TryGet(collection.GetEnumerator(), argument, predicate, out result);
+        }
+
+        public static bool TryGet<TValue, TArgument, TEnumerator>(TEnumerator enumerator, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result) where TEnumerator : IEnumerator<TValue>
+        {
             if (predicate == null) throw new ArgumentNullException(nameof(predicate));
 
-            foreach (TValue element in collection)
+            while (enumerator.MoveNext())
             {
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
+                TValue element = enumerator.Current;
 
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TValue, TArgument>(IReadOnlyList<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            for (int i = 0; i < collection.Count; i++)
-            {
-                TValue element = collection[i];
-
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TKey, TValue, TArgument>(Dictionary<TKey, TValue> collection, TArgument argument, CollectionPredicate<TKey, TArgument> predicate, out TKey result)
-        {
-            foreach (KeyValuePair<TKey, TValue> pair in collection)
-            {
-                if (predicate(pair.Key, argument))
-                {
-                    result = pair.Key;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TKey, TValue, TArgument>(Dictionary<TKey, TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            foreach (KeyValuePair<TKey, TValue> pair in collection)
-            {
-                if (predicate(pair.Value, argument))
-                {
-                    result = pair.Value;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TValue, TArgument>(HashSet<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            foreach (TValue element in collection)
-            {
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TValue, TArgument>(Stack<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            foreach (TValue element in collection)
-            {
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TValue, TArgument>(Queue<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            foreach (TValue element in collection)
-            {
                 if (predicate(element, argument))
                 {
                     result = element;
