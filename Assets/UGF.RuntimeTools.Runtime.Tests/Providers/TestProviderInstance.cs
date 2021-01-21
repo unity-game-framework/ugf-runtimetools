ccabd6be88dec618de8ce8ff0f43b3634ca410cd
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-21 20:46:20 +0300

    Add tests

---
+++ Assets/UGF.RuntimeTools.Runtime.Tests/Providers/TestProviderInstance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using UGF.RuntimeTools.Runtime.Providers;
+
+namespace UGF.RuntimeTools.Runtime.Tests.Providers
+{
+    public class TestProviderInstance
+    {
+        [TearDown]
+        public void Teardown()
+        {
+            ProviderInstance<IProvider>.Clear();
+        }
+
+        [Test]
+        public void GetNull()
+        {
+            Assert.Throws<InvalidOperationException>(() => Assert.Null(ProviderInstance<IProvider>.Instance));
+        }
+
+        [Test]
+        public void SetNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ProviderInstance<IProvider>.Instance = null);
+        }
+
+        [Test]
+        public void SetAndGetAndClear()
+        {
+            var provider = new Provider<string, string>();
+
+            ProviderInstance<IProvider>.Instance = provider;
+
+            Assert.True(ProviderInstance<IProvider>.HasInstance);
+            Assert.AreEqual(provider, ProviderInstance<IProvider>.Instance);
+
+            ProviderInstance<IProvider>.Clear();
+
+            Assert.False(ProviderInstance<IProvider>.HasInstance);
+        }
+    }
+}
