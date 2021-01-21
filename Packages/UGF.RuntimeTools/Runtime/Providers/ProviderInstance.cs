0d2966362786d704cf81ea3005b1cdb4309a8074
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-21 20:46:14 +0300

    Add ProviderInstance

---
+++ Packages/UGF.RuntimeTools/Runtime/Providers/ProviderInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UGF.RuntimeTools.Runtime.Providers
+{
+    public static class ProviderInstance<TProvider> where TProvider : class, IProvider
+    {
+        public static TProvider Instance { get { return m_instance ?? throw new InvalidOperationException("No value specified."); } set { m_instance = value ?? throw new ArgumentNullException(nameof(value)); } }
+        public static bool HasInstance { get { return m_instance != null; } }
+
+        private static TProvider m_instance;
+
+        public static bool Clear()
+        {
+            if (m_instance != null)
+            {
+                m_instance = null;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
