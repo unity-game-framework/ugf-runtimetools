33fd8833be1de721aba8b0c1b705997891861176
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-22 21:17:05 +0300

    Update ProviderInstance.cs

--- Packages/UGF.RuntimeTools/Runtime/Providers/ProviderInstance.cs
+++ Packages/UGF.RuntimeTools/Runtime/Providers/ProviderInstance.cs
@@ -2,22 +2,41 @@
 
 namespace UGF.RuntimeTools.Runtime.Providers
 {
-    public static class ProviderInstance<TProvider> where TProvider : class, IProvider
+    public static class ProviderInstance
     {
-        public static TProvider Instance { get { return m_instance ?? throw new InvalidOperationException("No value specified."); } set { m_instance = value ?? throw new ArgumentNullException(nameof(value)); } }
-        public static bool HasInstance { get { return m_instance != null; } }
+        public static IProvider<Type, IProvider> Providers { get { return m_providers; } set { m_providers = value ?? throw new ArgumentNullException(nameof(value)); } }
 
-        private static TProvider m_instance;
+        private static IProvider<Type, IProvider> m_providers = new Provider<Type, IProvider>();
 
-        public static bool Clear()
+        public static void Set<T>(T provider) where T : IProvider
         {
-            if (m_instance != null)
-            {
-                m_instance = null;
-                return true;
-            }
+            Set(typeof(T), provider);
+        }
 
-            return false;
+        public static void Set(Type type, IProvider provider)
+        {
+            m_providers.Remove(type);
+            m_providers.Add(type, provider);
+        }
+
+        public static T Get<T>() where T : IProvider
+        {
+            return m_providers.Get<T>(typeof(T));
+        }
+
+        public static IProvider Get(Type type)
+        {
+            return m_providers.Get(type);
+        }
+
+        public static bool TryGet<T>(out T provider) where T : IProvider
+        {
+            return m_providers.TryGet(typeof(T), out provider);
+        }
+
+        public static bool TryGet(Type type, out IProvider provider)
+        {
+            return m_providers.TryGet(type, out provider);
         }
     }
 }
