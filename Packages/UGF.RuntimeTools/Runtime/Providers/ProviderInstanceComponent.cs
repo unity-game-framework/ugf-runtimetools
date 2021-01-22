df063b89f395bb754e638052c60dedb3f34573f6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-22 21:17:25 +0300

    Add ProviderInstanceComponent

---
+++ Packages/UGF.RuntimeTools/Runtime/Providers/ProviderInstanceComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UGF.RuntimeTools.Runtime.Providers
+{
+    public abstract class ProviderInstanceComponent<TProvider> : ProviderInstanceComponentBase where TProvider : class, IProvider
+    {
+        protected override void OnAddProvider(IProvider<Type, IProvider> providers)
+        {
+            TProvider provider = OnCreateProvider();
+
+            providers.Add(typeof(TProvider), provider);
+        }
+
+        protected override void OnRemoveProvider(IProvider<Type, IProvider> providers)
+        {
+            providers.Remove(typeof(TProvider));
+        }
+
+        protected abstract TProvider OnCreateProvider();
+    }
+}
---
+++ Packages/UGF.RuntimeTools/Runtime/Providers/ProviderInstanceComponentBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.RuntimeTools.Runtime.Providers
+{
+    public abstract class ProviderInstanceComponentBase : MonoBehaviour
+    {
+        private void Start()
+        {
+            OnAddProvider();
+        }
+
+        private void OnDestroy()
+        {
+            OnRemoveProvider();
+        }
+
+        protected virtual void OnAddProvider()
+        {
+            OnAddProvider(ProviderInstance.Providers);
+        }
+
+        protected virtual void OnRemoveProvider()
+        {
+            OnRemoveProvider(ProviderInstance.Providers);
+        }
+
+        protected abstract void OnAddProvider(IProvider<Type, IProvider> providers);
+        protected abstract void OnRemoveProvider(IProvider<Type, IProvider> providers);
+    }
+}
