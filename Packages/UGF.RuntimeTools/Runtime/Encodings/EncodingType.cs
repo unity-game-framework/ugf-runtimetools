72971da8b041a9a17423d5c800ebaaef32b4a102
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-07-22 14:44:24 +0300

    Add EncodingUtility

---
+++ Packages/UGF.RuntimeTools/Runtime/Encodings/EncodingType.cs
@@ -0,0 +1,13 @@
+﻿namespace UGF.RuntimeTools.Runtime.Encodings
+{
+    public enum EncodingType
+    {
+        Default = 0,
+        Unicode = 1,
+        BigEndianUnicode = 2,
+        UTF7 = 3,
+        UTF8 = 4,
+        UTF32 = 5,
+        ASCII = 6
+    }
+}
