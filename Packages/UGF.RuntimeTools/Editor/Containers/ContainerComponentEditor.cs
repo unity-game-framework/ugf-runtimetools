b40d58cc0617c6626a4026d443184fe776ef690e
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-05 16:13:28 +0300

    Add ContainerComponent

---
+++ Packages/UGF.RuntimeTools/Editor/Containers/ContainerComponentEditor.cs
@@ -0,0 +1,34 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.RuntimeTools.Runtime.Containers;
+using UnityEditor;
+
+namespace UGF.RuntimeTools.Editor.Containers
+{
+    [CustomEditor(typeof(ContainerComponent), true)]
+    internal class ContainerComponentEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_listValues;
+
+        private void OnEnable()
+        {
+            m_listValues = new ReorderableListDrawer(serializedObject.FindProperty("m_values"));
+            m_listValues.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listValues.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listValues.DrawGUILayout();
+            }
+        }
+    }
+}
---
+++ Packages/UGF.RuntimeTools/Runtime/Containers/IContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace UGF.RuntimeTools.Runtime.Containers
+{
+    public interface IContainer : IEnumerable
+    {
+        int Count { get; }
+
+        T Get<T>() where T : class;
+        object Get(Type type);
+        bool TryGet<T>(out T value) where T : class;
+        bool TryGet(Type type, out object value);
+    }
+}
