60d5958e56ddde149fe878904ce8b8edd55d0b20
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-26 17:06:55 +0300

    Add TableAsset

---
+++ Packages/UGF.RuntimeTools/Runtime/Tables/ITable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UGF.EditorTools.Runtime.Ids;
+
+namespace UGF.RuntimeTools.Runtime.Tables
+{
+    public interface ITable
+    {
+        IEnumerable<ITableEntry> Entries { get; }
+
+        public T Get<T>(GlobalId id) where T : class, ITableEntry;
+        public ITableEntry Get(GlobalId id);
+        public bool TryGet<T>(GlobalId id, out T entry) where T : class, ITableEntry;
+        public bool TryGet(GlobalId id, out ITableEntry entry);
+    }
+}
