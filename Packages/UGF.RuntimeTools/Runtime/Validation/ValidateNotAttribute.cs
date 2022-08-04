bf26d482859d97bf10346ff4ad1aa7641132f318
Vorobey <vorobyovvorobyov@gmail.com>
2022-08-04 22:34:35 +0300

    Add ValidateNotAttribute

---
+++ Packages/UGF.RuntimeTools/Runtime/Validation/ValidateNotAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.RuntimeTools.Runtime.Validation
+{
+    public class ValidateNotAttribute : ValidateAttribute
+    {
+        public object Value { get; }
+
+        public ValidateNotAttribute(object value) : this(value, typeof(object))
+        {
+        }
+
+        public ValidateNotAttribute(object value, Type targetType) : base(targetType)
+        {
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        protected override ValidateResult OnValidate(object value, IContext context)
+        {
+            return !Value.Equals(value) ? ValidateResult.Valid : ValidateResult.CreateInvalid($"Value must not be equal to '{Value}'.");
+        }
+    }
+}
