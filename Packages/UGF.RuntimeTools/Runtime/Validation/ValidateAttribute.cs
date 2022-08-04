1570b3ad2edc79b7d237609251c056f49609b639
Vorobey <vorobyovvorobyov@gmail.com>
2022-08-04 20:36:31 +0300

    Update ValidateAttribute.cs

--- Packages/UGF.RuntimeTools/Runtime/Validation/ValidateAttribute.cs
+++ Packages/UGF.RuntimeTools/Runtime/Validation/ValidateAttribute.cs
@@ -6,12 +6,23 @@ namespace UGF.RuntimeTools.Runtime.Validation
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
     public class ValidateAttribute : Attribute
     {
+        public Type TargetType { get; }
+
+        public ValidateAttribute() : this(typeof(object))
+        {
+        }
+
+        public ValidateAttribute(Type targetType)
+        {
+            TargetType = targetType ?? throw new ArgumentNullException(nameof(targetType));
+        }
+
         public ValidateResult Validate(object value, IContext context)
         {
             if (value == null) throw new ArgumentNullException(nameof(value));
             if (context == null) throw new ArgumentNullException(nameof(context));
 
-            return OnValidate(value, context);
+            return TargetType.IsInstanceOfType(value) ? OnValidate(value, context) : ValidateResult.CreateInvalid($"Value must be of the specified type: '{TargetType}'.");
         }
 
         protected virtual ValidateResult OnValidate(object value, IContext context)
