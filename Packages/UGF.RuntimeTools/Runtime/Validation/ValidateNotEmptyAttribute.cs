9a25324ee656f27a9282bfe821c2a403fedb3b27
Vorobey <vorobyovvorobyov@gmail.com>
2022-08-05 15:14:35 +0300

    Add ValidateNotEmptyAttribute

---
+++ Packages/UGF.RuntimeTools/Runtime/Validation/ValidateNotEmptyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UGF.RuntimeTools.Runtime.Collections;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.RuntimeTools.Runtime.Validation
+{
+    public class ValidateNotEmptyAttribute : ValidateAttribute
+    {
+        public ValidateNotEmptyAttribute() : base(typeof(IEnumerable))
+        {
+        }
+
+        protected override ValidateResult OnValidate(object value, IContext context)
+        {
+            int count = CollectionsUtility.GetCount((IEnumerable)value);
+
+            return count > 0 ? ValidateResult.Valid : ValidateResult.CreateInvalid("Value must be not empty.");
+        }
+    }
+}
