d1f8ba30256773eb4cc4aeb17a40ab482c6f0ec3
Vorobey <vorobyovvorobyov@gmail.com>
2022-09-12 22:05:52 +0300

    Update ValidateReport.cs

--- Packages/UGF.RuntimeTools/Runtime/Validation/ValidateReport.cs
+++ Packages/UGF.RuntimeTools/Runtime/Validation/ValidateReport.cs
@@ -49,5 +49,27 @@ namespace UGF.RuntimeTools.Runtime.Validation
 
             return false;
         }
+
+        public bool TryGetByMemberResults(string memberName, ICollection<ValidateMemberResult> results)
+        {
+            if (string.IsNullOrEmpty(memberName)) throw new ArgumentException("Value cannot be null or empty.", nameof(memberName));
+            if (results == null) throw new ArgumentNullException(nameof(results));
+            if (!IsValid()) throw new ArgumentException("Value should be valid.");
+
+            bool result = false;
+
+            for (int i = 0; i < m_results.Count; i++)
+            {
+                ValidateMemberResult memberResult = m_results[i];
+
+                if (memberResult.Member.Name == memberName)
+                {
+                    result = true;
+                    results.Add(memberResult);
+                }
+            }
+
+            return result;
+        }
     }
 }
