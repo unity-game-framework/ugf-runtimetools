3351f1f02d8b975d33e2a57a848f99160a0c60dc
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-24 16:25:40 +0100

    Add Optional

---
+++ Packages/UGF.RuntimeTools/Runtime/Options/Optional.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UGF.RuntimeTools.Runtime.Options
+{
+    public readonly struct Optional<TValue>
+    {
+        public TValue Value { get { return HasValue ? m_value : throw new ArgumentException("Value not specified."); } }
+        public bool HasValue { get; }
+
+        public static Optional<TValue> Empty { get; } = new Optional<TValue>();
+
+        private readonly TValue m_value;
+
+        public Optional(TValue value)
+        {
+            m_value = value;
+
+            HasValue = true;
+        }
+
+        public static implicit operator TValue(Optional<TValue> result)
+        {
+            return result.Value;
+        }
+
+        public static implicit operator Optional<TValue>(TValue value)
+        {
+            return new Optional<TValue>(value);
+        }
+
+        public static implicit operator bool(Optional<TValue> result)
+        {
+            return result.HasValue;
+        }
+    }
+}
