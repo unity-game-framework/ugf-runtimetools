6b82ece703aecc16be13c8bb57e24bd7219963cc
Vorobey <vorobyovvorobyov@gmail.com>
2023-06-17 18:38:49 +0200

    Change ITable

--- Packages/UGF.RuntimeTools/Runtime/Tables/ITable.cs
+++ Packages/UGF.RuntimeTools/Runtime/Tables/ITable.cs
@@ -7,9 +7,13 @@ namespace UGF.RuntimeTools.Runtime.Tables
     {
         IEnumerable<ITableEntry> Entries { get; }
 
-        public T Get<T>(GlobalId id) where T : class, ITableEntry;
-        public ITableEntry Get(GlobalId id);
-        public bool TryGet<T>(GlobalId id, out T entry) where T : class, ITableEntry;
-        public bool TryGet(GlobalId id, out ITableEntry entry);
+        T GetByName<T>(string name) where T : ITableEntry;
+        ITableEntry GetByName(string name);
+        bool TryGetByName<T>(string name, out T entry) where T : class, ITableEntry;
+        bool TryGetByName(string name, out ITableEntry entry);
+        T Get<T>(GlobalId id) where T : class, ITableEntry;
+        ITableEntry Get(GlobalId id);
+        bool TryGet<T>(GlobalId id, out T entry) where T : class, ITableEntry;
+        bool TryGet(GlobalId id, out ITableEntry entry);
     }
 }
