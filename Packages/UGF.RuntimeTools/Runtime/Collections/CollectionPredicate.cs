ddcc5d47639714b7bbd1c1d072afe3e91e119f75
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-18 15:19:52 +0300

    Remove collection extensions

--- Packages/UGF.RuntimeTools/Runtime/Collections/CollectionPredicate.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace UGF.RuntimeTools.Runtime.Collections
-{
-    public delegate bool CollectionPredicate<in TValue, in TArgument>(TValue value, TArgument argument);
-}
--- Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsUtility.cs
+++ Packages/UGF.RuntimeTools/Runtime/Collections/CollectionsUtility.cs
@@ -1,57 +1,10 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace UGF.RuntimeTools.Runtime.Collections
 {
     public static class CollectionsUtility
     {
-        public static bool TryGet<TValue, TArgument, TCollection>(TCollection collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, CollectionIndexer<TValue, TCollection> indexer, int count, out TValue result)
-        {
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-            if (indexer == null) throw new ArgumentNullException(nameof(indexer));
-
-            for (int i = 0; i < count; i++)
-            {
-                TValue element = indexer(collection, i);
-
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
-        public static bool TryGet<TValue, TArgument>(IEnumerable<TValue> collection, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result)
-        {
-            if (collection == null) throw new ArgumentNullException(nameof(collection));
-
-            return TryGet(collection.GetEnumerator(), argument, predicate, out result);
-        }
-
-        public static bool TryGet<TValue, TArgument, TEnumerator>(TEnumerator enumerator, TArgument argument, CollectionPredicate<TValue, TArgument> predicate, out TValue result) where TEnumerator : IEnumerator<TValue>
-        {
-            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
-
-            while (enumerator.MoveNext())
-            {
-                TValue element = enumerator.Current;
-
-                if (predicate(element, argument))
-                {
-                    result = element;
-                    return true;
-                }
-            }
-
-            result = default;
-            return false;
-        }
-
         public static int GetCount(IEnumerable enumerable)
         {
             if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
