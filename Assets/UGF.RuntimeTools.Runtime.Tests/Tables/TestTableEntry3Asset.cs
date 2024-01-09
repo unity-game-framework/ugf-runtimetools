f20a81486b67f538984ba256bf8deaa198106fdc
Vorobey <vorobyovvorobyov@gmail.com>
2024-01-09 19:11:42 +0300

    Add tests

---
+++ Assets/UGF.RuntimeTools.Runtime.Tests/Tables/TestTableEntry3Asset.cs
@@ -0,0 +1,10 @@
+﻿using UGF.RuntimeTools.Runtime.Tables;
+using UnityEngine;
+
+namespace UGF.RuntimeTools.Runtime.Tests.Tables
+{
+    [CreateAssetMenu(menuName = "Tests/TestTableEntry3Asset")]
+    public class TestTableEntry3Asset : TableAsset<TestTableEntry3>
+    {
+    }
+}
