114344de7b8203ed67b8ddb4d181ddf220669365
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-04-04 18:13:56 +0300

    Change tests

---
+++ Assets/UGF.RuntimeTools.Editor.Tests/Tables/TestTableTreeDrawer.cs
@@ -0,0 +1,56 @@
+﻿using UGF.RuntimeTools.Editor.Tables;
+using UGF.RuntimeTools.Runtime.Options;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.RuntimeTools.Editor.Tests.Tables
+{
+    public class TestTableTreeDrawer : TableTreeDrawer
+    {
+        public TestTableTreeDrawer(SerializedObject serializedObject, TableTreeOptions options) : base(serializedObject, options)
+        {
+        }
+
+        protected override void OnDrawToolbar()
+        {
+            base.OnDrawToolbar();
+
+            GUILayout.Button("Test");
+        }
+
+        protected override void OnDrawTable()
+        {
+            base.OnDrawTable();
+
+            GUILayout.Button("Test");
+        }
+
+        protected override void OnDrawFooter()
+        {
+            base.OnDrawFooter();
+
+            GUILayout.Button("Test");
+        }
+
+        protected override void OnMenuCreate(GenericMenu menu)
+        {
+            base.OnMenuCreate(menu);
+
+            menu.AddItem(new GUIContent("TEST"), false, () => { });
+        }
+
+        protected override void OnContextMenuHeaderCreate(GenericMenu menu, Optional<TableTreeColumnOptions> column)
+        {
+            base.OnContextMenuHeaderCreate(menu, column);
+
+            menu.AddItem(new GUIContent("TEST"), false, () => { });
+        }
+
+        protected override void OnContextMenuCreate(GenericMenu menu)
+        {
+            base.OnContextMenuCreate(menu);
+
+            menu.AddItem(new GUIContent("TEST"), false, () => { });
+        }
+    }
+}
---
+++ Assets/UGF.RuntimeTools.Editor.Tests/Tables/TestTableTreeWindow.cs
@@ -0,0 +1,15 @@
+﻿using UGF.RuntimeTools.Editor.Tables;
+using UGF.RuntimeTools.Runtime.Tests.Tables;
+using UnityEditor;
+
+namespace UGF.RuntimeTools.Editor.Tests.Tables
+{
+    [TableTreeWindow(typeof(TestTableEntry3Asset))]
+    public class TestTableTreeWindow : TableTreeWindow
+    {
+        protected override TableTreeDrawer OnCreateDrawer(SerializedObject serializedObject, TableTreeOptions options)
+        {
+            return new TestTableTreeDrawer(serializedObject, options);
+        }
+    }
+}
