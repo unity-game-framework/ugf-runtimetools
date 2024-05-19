c3b93e82e80f678e938bee17a0810c13fa024ff4
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2024-05-19 16:11:13 +0300

    Change tests

--- Assets/UGF.RuntimeTools.Runtime.Tests/Tables/TestTableEntryDropdownAsset.cs
+++ Assets/UGF.RuntimeTools.Runtime.Tests/Tables/TestTableEntryDropdownAsset.cs
@@ -7,9 +7,12 @@ namespace UGF.RuntimeTools.Runtime.Tests.Tables
     [CreateAssetMenu(menuName = "Tests/TestTableEntryDropdownAsset")]
     public class TestTableEntryDropdownAsset : ScriptableObject
     {
-        [TableEntryDropdown]
+        [TableEntryDropdown(typeof(TestTableEntry2Asset))]
         [SerializeField] private GlobalId m_id;
+        [TableEntryDropdown]
+        [SerializeField] private GlobalId m_id2;
 
         public GlobalId ID { get { return m_id; } set { m_id = value; } }
+        public GlobalId ID2 { get { return m_id2; } set { m_id2 = value; } }
     }
 }
